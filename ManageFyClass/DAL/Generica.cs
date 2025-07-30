2e2967868aa806e7df690938a093a13e8f8b6088
Isaac Loiola <isaackopi@icloud.com>
2025-07-30 16:24:23 -0300

    Classe Categoria e Transacao feitas!

---
+++ ManageFyClass/DAL/Generica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageFyClass.DAL
+{
+    class Generico<T> where T : class
+    {
+        protected readonly Context contexto;
+
+        public Generico(Context contexto)
+        {
+            this.contexto = contexto;
+        }
+    }
+}
